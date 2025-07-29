ee9822c8ee6bc13b2e9b50cdbce3f571da5e6e06
A41K <arokandris@gmail.com>
2025-07-29 19:25:04 +0200

    v1.21
    - Added FIGYELMEZTETŐ (mert szerintem kellett)
    - Changed a felső fontját az egésznek hogy jobb legyen

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
+    var popup = document.getElementById('disclaimer-popup');
+    var closeButton = document.getElementsByClassName('close-button')[0];
+
+    // Show the popup when the page loads
+    popup.style.display = 'block';
+
+    // When the user clicks on <span> (x), close the popup
+    closeButton.onclick = function() {
+        popup.style.display = 'none';
+    }
+
+    // When the user clicks anywhere outside of the popup, close it
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = 'none';
+        }
+    }
+
+
     const wordBoxes = document.querySelectorAll('.word-box');
 
     wordBoxes.forEach(box => {
